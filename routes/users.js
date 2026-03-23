ce6d032ba88c7c2727ef7cf4caa309e7e457c4ed
thi-tr <thi.t2103@gmail.com>
2026-03-23 11:34:25 -0400

    Worked on users route and started a new quiz route and view (video updates not included)

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,16 @@ router.get('/', (req, res)=>{
 router.get('/new', (req, res)=>{ // /users/new
     res.send('User New Form');
 });
+//router.get('/:id', (req, res)=>{
+// res.send(`Getting User Data: ${req.params.id}`);
+//});
+
+router.route('/:id').get((req, res)=>{
+ res.send(`Getting User data for id: ${req.params.id}`);
+}).delete((req, res)=>{
+res.send(`Deleting User data for id: ${req.params.id}`);
+}).put((req, res)=>{
+res.send(`Updating User data for id: ${req.params.id}`);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/users')
+const quizRouter = require('./routes/quiz')
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/users', userRouter)
 
+app.use('/users', userRouter);
+app.use('/quiz', quizRouter);
 app.get('/solution', (req, res) =>{
-  let num1 = parseInt(req.query('num1'));
-  let num2 = parseInt(req.query('num2'));
+  let num1 = parseInt(req.query['num1']);
+  let num2 = parseInt(req.query['num2']);
   let total = num1 + num2;
   res.send(`<h2>${num1} + ${num2} = ${total}</h2>`);
 });
