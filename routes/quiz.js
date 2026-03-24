67bc21cc7c696c73fcaf350c0630767146ff23ba
thi-tr <thi.t2103@gmail.com>
2026-03-24 14:42:05 -0400

    Made function for shuffle and get random part

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const router = express.Router();
-
+const {readFile} = require('fs').promises;
 //work goes here
 
+router.get("/", (req,res)=>{
+    //Get 4 words, with their pos and def and send back to their other page
 
+    //send those back and redner quiz.ejs
+});
 
+let getWords = async ()=>{
+    //get a random part of speech
+    let randomPart = getRandomPart();
+    //based on that, pick 4 words that match
+}
+let getRandomPart = ()=>{
+    let parts = ['noun', 'verb', 'adjective'];
+    let randomIndex = Math.floor(Math.random()*parts.length);
+    let randomPart = parts[randomIndex];
+    return randomPart;
+}
 
-
+let shuffle = (array) =>{
+    //fisher Yates algorithm
+    for(let i = 0; i<array.length - 1; i--){
+        let randomNumber = Math.floor(Math.random()*(i+1));
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
+    }
+}
 
 
 module.exports = router
